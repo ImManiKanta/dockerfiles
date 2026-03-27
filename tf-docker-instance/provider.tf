1e387c7813f93b6c4f9104f472b407003e54c6b3
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-27 19:35:59 +0530

    docker

---
+++ tf-docker-instance/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0"
+    }
+  }
+}
+
+/*   backend "s3" {
+    bucket = "tf-remotestate-mani-dev"
+    key    = "docker-instance"
+    region = "us-east-1"
+    use_lockfile = true # This is the default behavior now
+    encrypt        = true
+  }
+} */
+
+provider "aws" {
+  region = "us-east-1"
+}
+
