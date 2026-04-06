6c19e5858c1a3b5b39eb7639c107bc6f0de3bb27
ImManiKanta <chinthamanikanta1@gmail.com>
2026-04-06 18:03:09 +0530

    ekscluster role created and userdata updated

--- tf-docker-instance/docker-install-diskgrow.sh
+++ tf-docker-instance/docker-install-diskgrow.sh
@@ -12,3 +12,27 @@ sudo usermod -aG docker ec2-user
 sudo growpart /dev/nvme0n1 4
 sudo lvextend -L +30G /dev/RootVG/varVol
 sudo xfs_growfs /var
+
+#eksctl install
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check 
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+#kubectl install
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.2/2026-02-27/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+#create eks cluster
+cd /home/ec2-user/
+git clone https://github.com/ImManiKanta/eksctl.git
+cd eksctl/
+eksctl create cluster --config-file=eks.yaml >> EKSClusterOutput.log
